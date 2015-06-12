31df036b556d577a5bbdec03508db06e1dff0e9b
Gregory Jefferis <jefferis@gmail.com>
2015-06-12 20:51:00 +0100

    test shortest_bridging_seq

--- tests/testthat/test-transformation.r
+++ tests/testthat/test-transformation.r
@@ -49,6 +49,18 @@ test_that("bridging graph and friends work",{
   # maybe dangerous to assume sort order ...
   df=df[match(df$path, df2$path),]
   expect_equal(df2, df)
+
+  baseline=list(structure(file.path(td, "/Library/Frameworks/R.framework/Versions/3.2/Resources/library/nat.flybrains/extdata/bridgingregistrations/FCWB_IS2.list"),
+    swapped = TRUE))
+  expect_equal(shortest_bridging_seq('FCWB', 'IS2'), baseline)
+  expect_is(shortest_bridging_seq('FCWB', 'IBN'), 'list')
+  expect_warning(fcwb_ibn<-shortest_bridging_seq('FCWB', 'IBN', imagedata = T),
+                 'very slow for image data')
+  bl=list(file.path(td,"/Users/jefferis/Library/Application Support/rpkg-nat.flybrains/regrepos/BridgingRegistrations/JFRC2_FCWB.list"),
+       structure(file.path(td,"/Library/Frameworks/R.framework/Versions/3.2/Resources/library/nat.flybrains/extdata/bridgingregistrations/JFRC2_IBNWB.list"), swapped = TRUE),
+       structure(file.path(td,"/Library/Frameworks/R.framework/Versions/3.2/Resources/library/nat.flybrains/extdata/bridgingregistrations/IBNWB_IBN.list"), swapped = TRUE))
+  expect_equal(fcwb_ibn, bl)
+  expect_error(shortest_bridging_seq('FCWB', 'crumble'))
 })
 
 test_that("we can find bridging registrations",{
