4296c7c9c7c1efce1ef7d4ba38821e4b5b65f654
Gregory Jefferis <jefferis@gmail.com>
2020-04-17 08:38:59 +0100

    coverage: additional templatebrain tests

--- tests/testthat/test-templatebrain.R
+++ tests/testthat/test-templatebrain.R
@@ -1,4 +1,10 @@
 test_that("templatebrain works", {
-  expect_is(IS2 <- templatebrain("IS2"), 'templatebrain')
+  expect_is(IS2 <- templatebrain("IS2", dims=c(30,40,50), note="Test field"),
+            'templatebrain')
   expect_null(IS2$origin)
+
+  expect_equal(as.templatebrain(IS2), IS2)
+
+  expect_error(as.templatebrain("rhubarb.nrrd"))
+  expect_equal(origin(IS2), c(0,0,0))
 })
